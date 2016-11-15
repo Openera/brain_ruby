708e24ce30a43a09ff095063d101bd609f49329a
StuartMacdonald <smacdonald@liveqos.com>
2016-11-15 12:51:30 -0500

    parameter for port

--- lib/brain_ruby.rb
+++ lib/brain_ruby.rb
@@ -4,17 +4,21 @@ require "cgi"
 
 module BrainRuby
   class Brain
-    include HTTParty
+    #include HTTParty
 
     def initialize(config = {})
       @api_key = config[:key] || raise('No API key!')
       @base_uri = config[:base_uri] || raise("Base URI expected!")
+      @port = config[:port] || raise("Port number expected!")
     end
 
     def url(location, destination)
-      response = HTTParty.get("#{@base_uri}:8443/rest/url?apiKey=#{@api_key}&location=#{location}&destination=#{CGI.escape(destination)}")
-
-      response.body
+      response = HTTParty.get("#{@base_uri}:#{@port}/rest/url?apiKey=#{@api_key}&location=#{location}&destination=#{CGI.escape(destination)}")
+      if response.code == 200
+        response.body
+      else
+        nil
+      end
     end
   end
 end
